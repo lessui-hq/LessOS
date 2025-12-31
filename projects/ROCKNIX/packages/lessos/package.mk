9d568c93ed6630ac607b55625c6f1f082d0fac47
Nick Chapman <nchapman@fastmail.com>
2025-12-30 19:36:54 -0800

    Move partition setup and extraction to autostart phase
    - Add 050-lessos autostart script: mounts SD2, creates LESSUI partition,
      extracts archives
    - Runs during rocknix-autostart with splash screen visible
    - Simplify lessos-boot to only find and launch init.sh
    - Remove lessos-automount service (functionality moved to autostart)
    - Change "Extracting LessUI.7z" to "Installing LessUI..." for better UX

--- projects/ROCKNIX/packages/lessos/package.mk
+++ projects/ROCKNIX/packages/lessos/package.mk
@@ -11,16 +11,20 @@ PKG_LONGDESC="LessOS boot system - launches LessUI from lessos/init.sh"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  # Install scripts
+  # Install boot script
   mkdir -p ${INSTALL}/usr/bin
   cp ${PKG_DIR}/sources/lessos-boot ${INSTALL}/usr/bin/
   cp ${PKG_DIR}/sources/lessos-automount ${INSTALL}/usr/bin/
   chmod 0755 ${INSTALL}/usr/bin/lessos-boot
   chmod 0755 ${INSTALL}/usr/bin/lessos-automount
+
+  # Install autostart script (runs during boot with splash visible)
+  mkdir -p ${INSTALL}/usr/lib/autostart/common
+  cp ${PKG_DIR}/autostart/050-lessos ${INSTALL}/usr/lib/autostart/common/
+  chmod 0755 ${INSTALL}/usr/lib/autostart/common/050-lessos
 }
 
 post_install() {
-  # Enable LessOS services
-  enable_service lessos-automount.service
+  # Enable LessOS boot service (autostart script handles SD mounting and partition setup)
   enable_service lessos-boot.service
 }
